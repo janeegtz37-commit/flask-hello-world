a8140712eaf7481d98d581402c928a4f0bd6a178
janeegtz37-commit <janeegtz37@gmail.com>
2026-02-23 10:51:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 def productos():
  import mysql.connector
  mydb = mysql.connector.connect(
-   host="46.28.42.226",
-   user="u760464709_24005242_usr",
-   password="u7?Jpkt>Y*E7",
-   database="u760464709_24005242_bd"
+  host="46.28.42.226",
+  user="u760464709_24005242_usr",
+  password="u7?Jpkt>Y*E7",
+  database="u760464709_24005242_bd"
 )
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM productos")
